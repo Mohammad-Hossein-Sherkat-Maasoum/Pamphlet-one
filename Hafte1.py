203692cf2c08afb11d2b4ca2cfc50caeec44308c
AmirCollider <101126826+Mohammad-Hossein-Sherkat-Maasoum@users.noreply.github.com>
2022-12-18 13:53:39 +0330

    Add files via upload

---
+++ Hafte1.py
@@ -0,0 +1,88 @@
+## X = Integer
+
+X = 3+7
+
+X = X + 2
+
+print(X * 2)
+
+## Calculate the circumference of a circle
+
+Pi = 3.14
+Radius = 3
+Diameter = Radius * 2
+Environment = Diameter * Pi
+print(Environment)
+
+## String = ("jady") // String = "jady"
+
+name = ("jady")
+
+age = 40
+
+print("the ega of" , name , "is" , age)
+
+## A = Float
+
+A = 2.0
+
+print(A)
+
+## The answer to division in Python is always float
+
+print(5 / 2)
+
+##
+
+print(24 - 13)
+
+## If in addition or subtraction of numbers in Python
+# is one of the float numbers, the answer will be a decimal number
+
+x = 2.0
+
+y = 1
+
+z=x+y
+
+print(z)
+
+## We can do more complex calculations in Python
+
+x = (24 * 2) / 3
+
+y = 2.5
+
+print(x + (y * 2))
+
+## It is very important to respect uppercase
+# and lowercase letters in the naming of variables
+
+x = "hello there"
+
+print(x)
+
+## In Python, the codes are executed linearly
+#and we can change the variables in different places
+
+x = "dog"
+
+y = "cat"
+
+x = 2
+
+print(x, y)
+
+## Calculate the length and width of a rectangle in Python
+
+length = 10
+
+width = 5
+
+area = length * width
+
+Environment = (length+width)*2
+
+print("area = " , area)
+
+print("Environment = " , Environment)
